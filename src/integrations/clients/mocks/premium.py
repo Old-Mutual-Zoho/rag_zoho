cf0bcd2fd3fb3eb7fe6952bc0aeb5c5250ca7000
Layika <zulayikanalukenge@gmail.com>
2026-03-03 00:38:07 +0300

    integrations(mocks): add premium client entrypoint

---
+++ src/integrations/clients/mocks/premium.py
@@ -0,0 +1,5 @@
+"""Backward-compatible exports for premium mocks."""
+
+from .premium_mocks.premium import MockPremiumClient, mock_premium_client
+
+__all__ = ["MockPremiumClient", "mock_premium_client"]
